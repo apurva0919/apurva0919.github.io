73f3474982d1be0c59161b8736a3cf1fb35896c4
Apurva Jain <apurva0919@gmail.com>
2019-09-13 11:37:46 -0500

    Final

--- jquery.js
+++ jquery.js
@@ -4,4 +4,20 @@ $('.js-nav-icon').click(function(){
     var nav = $('.js-sidebar');
     nav.slideToggle(200);
 });
+
+/* $(window).scroll(function() {
+    var position = $(this).scrollTop();
+
+    $('.section').each(function() {
+        var target = $(this).offset().top;
+        var id = $(this).attr('id');
+        console.log(id);
+
+if (position >= target) {
+    $('#navigation > ul > li > a').removeClass('active');
+    $('#navigation > ul > li > a[href=#' + id + ']').addClass('active');
+}
+});
+}); */
+
 });
